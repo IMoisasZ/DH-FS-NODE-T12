e47dbadb32e13a47857ff6d4a0e4745ec8d759d2
Moises Santos <mopri08@gmail.com>
2021-01-11 21:31:26 -0300

    voltando as aulas fazendo uma dinamismo na criando cards automaticos

---
+++ Aula_21/Data/card.js
@@ -0,0 +1,8 @@
+const path = require("path")
+let cards = [
+    {'imagem': "/images/card_1.jpg", 'titulo': "Macarrão", 'texto': "Macarrão Patati - Patata", 'botao': "Ver mais"},
+    {'imagem': "/images/card_2.jpg", 'titulo': "Macarrão Integral", 'texto': "Macarrão Patati - Patata Integral", 'botao': "Ver mais"},
+    {'imagem': "/images/card_3.jpg", 'titulo': "Torta de carne", 'texto': "Torta de carne", 'botao': "Ver mais"}
+]
+
+module.exports = cards
