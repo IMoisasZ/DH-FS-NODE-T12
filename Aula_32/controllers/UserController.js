6cb79a6dd84b5dd4e41cb04257a77bb481be90d2
Moises Santos <mopri08@gmail.com>
2021-03-15 22:48:30 -0300

    arrumando o autenticate, limit itens e busca na pagina

---
+++ Aula_32/controllers/UserController.js
@@ -0,0 +1,54 @@
+const { User } = require('../models/')
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  create(req, res, next){
+    res.render('create-user');
+  },
+
+  async save(req, res, next){
+    /*  criptografando a senha */
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+    /* criando objeto para enviar adicionar no array users */
+    let user = { ...req.body };
+    /* adicionando objeto dentro do banco de dados */
+    await User.create(user)
+
+    
+    res.render('create-user', { added: true });
+  },
+
+  login(req, res, next){
+    res.render('login');
+  },
+
+  async authenticate(req, res, next){
+    let { email, password } = req.body;
+    let user = await User.findOne({ where: { email }});
+
+    if(!user){
+      return res.render('login', { notFound: true });
+    }
+
+    if(!bcrypt.compareSync(password, user.password)){
+      return res.render('login', { notFound: true });
+    }
+    
+    // removendo propriedade password para que o usuario logado nao trafegue com sua senha
+    // let { password: pass, ...userWithoutPassword } = user; 
+
+    user.password = undefined
+    // criando sessao contendo informacoes do usuario que ira se logar
+    // req.session.user = userWithoutPassword;
+
+    req.session.user = user;
+
+    res.redirect('/recipes');
+  },
+
+  logout(req, res, next){
+    req.session.destroy();
+    res.redirect('/');
+  }
+}
