43bd56e5fc306661ecc0d8881289f6f37cdf4f25
Moises Santos <mopri08@gmail.com>
2021-04-14 20:46:37 -0300

    criando rotas para acesso externo

--- API-todo-list/src/routes/index.js
+++ API-todo-list/src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
 const router = Router()
-const TaskController = require('../controller/PhotoController')
+const PhotoController = require('../controller/PhotoController')
 
-router.get("/api/photo",)
+router.get("/api/photo/:limit?", PhotoController.index)
+router.get("/api/photo/:id", PhotoController.getById)
 
 module.exports = router;
