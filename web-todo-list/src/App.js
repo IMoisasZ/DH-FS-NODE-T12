3b75c468f13d41207a6604cb17a6411c64880dc3
Moises Santos <mopri08@gmail.com>
2021-04-19 22:03:29 -0300

    Inicio das aulas de react

---
+++ web-todo-list/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import TaskForm from './components/TaskForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <Header />
+        <div className="main">
+      </div>
+        <TaskForm />
+      </div>
+    </div> 
+  );
+}
+export default App;
